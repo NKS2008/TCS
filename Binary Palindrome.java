1214c1876b3f92379d07e4a69b8c9812fc3a5f65
S N KAVYA <nagakavyas@gmail.com>
2026-08-13 21:54:38 +0530

    Add Binary Palindrome checker
    Implement a program to check if a binary representation of a number is a palindrome.

---
+++ Binary Palindrome.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s1 = "";
+        String s2 = "";
+        while(n>0){
+            s1 = s1 + n%2;
+            s2 = n%2 + s2;
+            n = n/2;
+        }
+        if(s1.equals(s2)){
+            System.out.println("yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+}
+
