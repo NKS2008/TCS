b66c5e53efd67893b04643acadc02c17e7ad1213
S N KAVYA <nagakavyas@gmail.com>
2026-08-15 10:47:52 +0530

    Add program to print all substrings of a string

---
+++ Substrings of string.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int n = s.length();
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                for(int k = i; k <= j; k++){
+                    System.out.print(s.charAt(k));
+                }
+                System.out.println();
+            }
+        }
+    }
+}
