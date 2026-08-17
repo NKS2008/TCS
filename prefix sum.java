b1590613bb886ce321ce51da34df3e06a59bbb12
S N KAVYA <nagakavyas@gmail.com>
2026-08-17 16:42:06 +0530

    Create prefix sum.java

---
+++ prefix sum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] a =new int[n];
+        for(int i = 0; i < n; i++){
+            a[i]=sc.nextInt();
+        }
+        int [] p = new int[n];
+        p[0] = a[0];
+        for(int i = 1; i < n; i++){
+            p[i] = p[i-1] + a[i];
+        }
+        for(int i = 0; i < n; i++){
+            System.out.println(p[i]);
+        }
+    }
+}
